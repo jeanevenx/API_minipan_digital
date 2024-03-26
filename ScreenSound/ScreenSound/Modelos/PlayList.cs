78c9afdedaa6bcaf72d1c29a07f222ab9c89520c
Jean Evenx Joseph <jean.joseph@grupopan.com>
2024-03-26 11:15:08 -0300

    Fun: Criar playlist

---
+++ ScreenSound/ScreenSound/Modelos/PlayList.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace ScreenSound.Modelos;
+
+internal class Playlist(string nome)
+{
+    public string Nome { get; set; } = nome;
+    public List<Music> ListaDeMusicas { get; } = new List<Music>();
+
+    public void AdicionarMusica(Music music)
+    {
+        ListaDeMusicas.Add(music);
+    }
+
+    public void ExibirPlaylist()
+    {
+
+        int count = 1;
+        Console.WriteLine($"Playlist: {Nome}");
+
+        foreach (var music in ListaDeMusicas) { 
+            Console.WriteLine($"{count++} - {music.Nome}");
+        }
+    }
+   
+
+}
