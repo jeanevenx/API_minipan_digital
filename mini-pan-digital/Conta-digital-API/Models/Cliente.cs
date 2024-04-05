00c21a9680cd4d299d263c26b238f1b82c57f49e
Jean Evenx Joseph <jean.joseph@grupopan.com>
2024-04-05 18:17:41 -0300

    Definição de FK

--- mini-pan-digital/Conta-digital-API/Models/Cliente.cs
+++ mini-pan-digital/Conta-digital-API/Models/Cliente.cs
@@ -5,13 +5,16 @@ namespace Conta_digital_API.Models;
 
 public class Cliente
 {
-    //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     [Key]
-    public Guid ClienteID {  get; set; } 
+    [Required]
+    public int Id {  get; set; }
+    [ForeignKey("Conta")]
+    public int ContaId { get; set; }
     [Required]
     [StringLength(30)]
     public string? Nome { get; set; }
     [Required]
-    [StringLength(11, ErrorMessage ="Tamanho do cpf deve ser igual a 11")]
+    [StringLength(15, ErrorMessage ="Tamanho do cpf deve ser igual a 15")]
     public string? Cpf { get; set ;}
+
 }
