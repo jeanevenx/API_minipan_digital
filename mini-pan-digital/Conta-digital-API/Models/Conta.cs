623040c24f48a1a31087976d81ac73e14f90b6b7
Jean Evenx Joseph <jean.joseph@grupopan.com>
2024-04-05 18:19:02 -0300

    Reestruturar a classe

--- mini-pan-digital/Conta-digital-API/Models/Conta.cs
+++ mini-pan-digital/Conta-digital-API/Models/Conta.cs
@@ -1,22 +1,27 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Conta_digital_API.Models;
 
+
 public class Conta
 {
-    
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
-    [Required]
-    public  Cliente Usuario { get; set; }
-    [MaxLength(8, ErrorMessage ="Tamanho não pode exceder 8")]
-    public  string Numero { get; private set; }
+
+    [MaxLength(9, ErrorMessage ="Tamanho não pode exceder 8")]
+    public  string Numero { get; set; }
     [MaxLength(4, ErrorMessage = "Tamanho não pode exceder 4")]
-    public  string Agencia { get; private set; }
+    public  string Agencia { get; set; }
 
     [DataType(DataType.Currency)]
-    public  double Saldo { get; private set; }
-    public  string Tipo { get; private set; }
-   
+    public  double Saldo { get; set; }
+    public  string Tipo { get; set; }
+
+    [Required]
+    public Cliente Usuario { get; set; }
+
 
     public Conta() {
         GerarContaCorrente();
@@ -28,10 +33,11 @@ public class Conta
         int digitoAleatorio = new Random().Next(1, 10);
         int numeroAleatorio = new Random().Next(1000, 10000);
 
-        int primeirosDigitos = 2370 - digitoAleatorio;
+        int primeirosDigitos = 230 - digitoAleatorio;
         int ultimosDigitos = (numeroAleatorio - digitoAleatorio) / 2;
+        var numeroComposto = $"{primeirosDigitos}" + $"{ultimosDigitos}" + "-" + $"{digitoAleatorio}";
 
-        Numero = "122454-9";
+        Numero = numeroComposto;
         Agencia = "0001";
         Saldo = 0;
         Tipo = "Corrente";
