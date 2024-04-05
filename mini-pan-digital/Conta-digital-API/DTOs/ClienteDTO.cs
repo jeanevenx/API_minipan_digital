7104937980f936c421a27b474383c760bef91832
Jean Evenx Joseph <jean.joseph@grupopan.com>
2024-04-05 18:20:52 -0300

    Realizar o dto das entidades Conta e Cliente

---
+++ mini-pan-digital/Conta-digital-API/DTOs/ClienteDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Conta_digital_API.DTOs
+{
+    public class ClienteDTO
+    {
+        [Required]
+        [StringLength(30)]
+        public string? Nome { get; set; }
+        [Required]
+        [StringLength(15, ErrorMessage = "Tamanho do cpf deve ser igual a 11")]
+        public string? Cpf { get; set; }
+    }
+}
---
+++ mini-pan-digital/Conta-digital-API/DTOs/CreateContaDTO.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Conta_digital_API.DTOs;
+
+public class CreateContaDTO
+{
+    [Required]
+    public ClienteDTO Cliente { get; set; }
+}
